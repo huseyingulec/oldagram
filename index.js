9fe1a9949c945bc7d99f6fcdf9603a141d273804
huseyingulec <itshuseyingulec@gmail.com>
2023-04-19 16:24:37 +0200

    created project

---
+++ index.js
@@ -0,0 +1,51 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21596
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 10987
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 15224
+    }
+]
+
+const feed = document.querySelector(".feed")
+const userPost = document.querySelector(".user-post")
+let listPost = ""
+for(let i=0; i<posts.length; i++){
+    listPost += `
+    <div class="user-info">
+        <img src="${posts[i].avatar}" class="user-avatar">
+        <div>
+            <p class="user-name">${posts[i].name}</p>
+            <p class="user-location">${posts[i].location}</p>
+        </div>
+    </div>
+        <img src="${posts[i].post}" class="user-post">
+        <img src="images/icon-heart.png" class="icon-heart">
+        <img src="images/icon-comment.png" class="icon-comment">
+        <img src="images/icon-dm.png" class="icon-dm">
+        <p class="post-likes">${posts[i].likes} likes</p>
+        <p><span class="user-id">${posts[i].username}</span> <span class="user-comment">${posts[i].comment}</span></p>
+    `
+}
+feed.innerHTML = listPost
